58f1bc2b5d1609a1f583ba54ec6677dc2e402fb6
Randa Alfarisyi <randaalfarisyi@gmail.com>
2020-05-19 12:20:23 +0700

    update url usermicroservice

--- src/main/java/com/doit/order/rest/Setting.java
+++ src/main/java/com/doit/order/rest/Setting.java
@@ -1,5 +1,5 @@
 package com.doit.order.rest;
 
 public class Setting {
-	final public static String userMicroserviceUrl = "http://localhost:8081/api/v1";
+	final public static String userMicroserviceUrl = "https://usermicroservice1.herokuapp.com/api/v1";
 }
